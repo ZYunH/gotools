5054b7bb505338eb004b16380a54b395d398a84f
ZYunH <zyunhjob@163.com>
2019-06-21 11:37:17 +0800

    Bump to 0.0.3

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"unsafe"
 )
 
+var sizeOfInt = unsafe.Sizeof(0)
+
 // Simple print function used for debug
 func DebugPrint(args ...interface{}) {
 	if args[len(args)-1] != "\r\n" {
 		args = append(args, "\r\n")
 	}
+
 	for _, v := range args {
-		if v != "\r\n" {
-			print(fmt.Sprint(v), " ")
-		} else {
+		switch v.(type) {
+		case string:
+			interfaceAddr := uintptr(unsafe.Pointer(&v))
+			dataPtr := interfaceAddr + sizeOfInt
+			dataAddr := *(*uintptr)(unsafe.Pointer(dataPtr)) // type:string
+
+			dataAsSlice := *(*[]byte)(unsafe.Pointer(dataAddr))
+			dataAsSliceLen := *(*int)(unsafe.Pointer(dataAddr + sizeOfInt))
+
+			if !(dataAsSlice[dataAsSliceLen-1] == 10 && dataAsSlice[dataAsSliceLen-2] == 13) {
+				print(fmt.Sprint(v), " ")
+			} else {
+				print(fmt.Sprint(v))
+			}
+		default:
 			print(fmt.Sprint(v))
 		}
-
 	}
 }
 
--- main_test.go
+++ main_test.go
@@ -5,4 +5,9 @@ import (
 )
 
 func TestDebugPrint(t *testing.T) {
+	DebugPrint("Test1", "Test2")
+	DebugPrint("=============")
+	DebugPrint("Test1", "\r\n", "Test2")
+	DebugPrint("=============")
+	DebugPrint("Test1\r\n", "Test2\r\n")
 }
